8c9e873ffe15e3ba5aebed13a4f51d8ef43e9b68
thsolari <gtcalsolari@gmail.com>
2024-03-27 10:03:40 -0300

    mudanca de tamanho do logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
             <div className="sm:mx-auto sm:max-w-md">
                 <Image
                     alt="Logo"
-                    height={48}
-                    width={48}
+                    height={58}
+                    width={58}
                     className="mx-auto w-auto"
                     src={"/images/logo.png"}
                 />
